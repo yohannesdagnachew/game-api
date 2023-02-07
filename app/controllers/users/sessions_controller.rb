02b0d3619a2c573284f331c5d596afa3872e7a46
Jo <jo@Jos-MacBook-Air.local>
2023-02-07 22:19:23 +0300

    sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+     render json: {
+        status: {
+          message: "Signed in successfully", code: 200, data: resource
+        }
+      }
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: {
+          message: "Signed out successfully", code: 200
+        }
+      }
+    else
+      render json: {
+        status: {
+          message: "Signed out failed", code: 401
+        }
+      }
+    end
+  end
 end
